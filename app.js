a80fe35626a76cf32476bde37889ddb8c83174d1
Arthur Dev <jm054653@gmail.com>
2025-06-09 21:06:34 -0300

    commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Presentinho pra você amor';
+
+function verificarAmor1() {
+    alert('A junção perfeita que deus poderia fazer.');
+}
+
+function verificarAmor2() {
+    alert('Tudo, eu te amo daqui até o infinito, passo todo meu tempo pensando em você, você é tudo pra mim, a razão do meu viver, toda escolha foi justamente para você, largar toda vida da gandaia, pra te amar, eu te amo infinitamente, minha futura esposa, mais uma vez, eu te amo demaissss.');
+}
+
+function verificarAmor3() {
+    cidade = prompt('Você me ama ? "Sim ou Não" ');
+    alert(`Ainda bem que tu disse sim, tá doida é  kkkkkk, EU TE AMO DEMAIS MEU AMOR!!!`);
+}
