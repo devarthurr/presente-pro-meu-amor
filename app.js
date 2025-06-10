19bbab861354051a68b74c48c256498b5e21b70d
Arthur Dev <jm054653@gmail.com>
2025-06-09 21:30:10 -0300

    commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ function verificarAmor1() {
 }
 
 function verificarAmor2() {
-    alert('Tudo, eu te amo daqui até o infinito, passo todo meu tempo pensando em você, você é tudo pra mim, a razão do meu viver, toda escolha foi justamente para você, largar toda vida da gandaia, pra te amar, eu te amo infinitamente, minha futura esposa, mais uma vez, eu te amo demaissss.');
+    alert('Tudo, eu te amo daqui até o infinito, passo todo meu tempo pensando em você, você é tudo pra mim, a razão do meu viver, toda escolha foi justamente para você, largar toda vida fútil, pra te amar, eu te amo infinitamente, minha futura esposa, mais uma vez, eu te amo demaissss.');
 }
 
 function verificarAmor3() {
